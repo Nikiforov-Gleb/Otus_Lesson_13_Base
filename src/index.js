a38e218645e2bfe3fe43fd54ba74c667f632fe32
Nikiforov-Gleb <gl.nikiforov@gmail.com>
2026-02-13 16:22:28 +0300

    Created structure of Weather app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-import { runApp } from "./runApp";
+import "./css/styles.css";
 
-runApp(document.body);
+(async function () {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      () => {
+        //position
+        window.location.href = "weather.html";
+      },
+      () => {
+        //error
+        init();
+      },
+    );
+  } else {
+    init();
+  }
+})();
+
+function init() {
+  const searchForm = document.querySelector("form");
+  const input = document.querySelector("#cityInput");
+  const submitButton = document.querySelector("#submitButton");
+  const clearButton = document.querySelector("#clearButton");
+
+  input.addEventListener("input", () => {
+    submitButton.disabled = !input.value.trim();
+  });
+
+  clearButton.addEventListener("click", () => {
+    input.value = "";
+    submitButton.disabled = true;
+    input.focus();
+  });
+
+  searchForm.addEventListener("submit", async (ev) => {
+    ev.preventDefault();
+
+    const formElement = ev.target;
+    const inputEl = formElement.querySelector("input");
+    const cityName = inputEl.value;
+
+    window.location.href = `weather.html?city=${encodeURIComponent(cityName)}`;
+  });
+}
